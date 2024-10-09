ca37fef56dfc5e56773fefec6b6d8e6995676ac1
Alexandros Stathakopoulos <alexandros.stathakopoulos@tum.de>
2024-10-09 09:03:21 +0200

    init commit

---
+++ src/example.py
@@ -0,0 +1,11 @@
+from my_math import *
+
+print("* Addition:")
+print(" If I add 5 and 8, I get " + str(add_numbers(5, 8)) + ".")
+print("  ")
+print("* Subtraction:")
+print(" If I subtract 5 from 8, I get " + str(subtract_numbers(8, 5)) + ".")
+print("  ")
+print("* Multiplication:")
+print(" If I multiply 5 and 8, I get " + str(multiply_numbers(5, 8)) + ".")
+print("  ")
---
+++ src/my_math.py
@@ -0,0 +1,10 @@
+
+def add_numbers(f1, f2):
+    return f1 + f2
+
+
+def subtract_numbers(f1, f2):
+    return f2 - f1
+
+def multiply_numbers(f1, f2):
+    return f1 * f2
