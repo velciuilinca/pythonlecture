2940a488f8e877d1af547bbbe0a216c2e228dcd4
ilincavelciu <ilincavelciu@gmail.com>
2024-10-09 10:46:50 +0200

    Python functions

--- test/test_example.py
+++ test/test_example.py
@@ -1,1 +1,12 @@
 # Your testing code
+from src.my_math import *
+
+def test_addition():
+    result = add_numbers(5,7)
+    assert result == 12
+def test_substraction():
+    result = subtract_numbers(12,10)
+    assert result == -2
+def test_multiplication():
+    result = multiply_numbers(5,7)
+    assert result == 35
